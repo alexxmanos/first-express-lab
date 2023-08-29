b606f141bd5f1f393b2ee6e83f520295e293e819
alexxmanos <amanotas327@gmail.com>
2023-08-29 10:02:11 -0500

    Complete Tip calcuator portion of lab

--- server.js
+++ server.js
@@ -12,4 +12,21 @@ app.get('greeting/:name', (req, res) => {
     res.send('Hello, ${req.params.name}')
 })
 
+// Tip Calculator //
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const total = Number (req.params.total);
+    const tipPercentage = Number ('0.${req.params.tipPercentage}');
+    const newTotal = total * tipPercentage;
+    
+  
+    res.send('${newTotal}')
+})
+
+
+
+
+
+
+
 app.listen(3000, () => console.log('Listening on port 3000'))
