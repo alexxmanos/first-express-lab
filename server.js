1b9bf0c3c293a73f0736ec379f927c4df8f48289
alexxmanos <amanotas327@gmail.com>
2023-08-29 10:33:55 -0500

    node modules removed

--- server.js
+++ server.js
@@ -16,12 +16,47 @@ app.get('greeting/:name', (req, res) => {
 
 app.get('/tip/:total/:tipPercentage', (req, res) => {
     const total = Number (req.params.total);
-    const tipPercentage = Number ('0.${req.params.tipPercentage}');
+    const tipPercentage = Number (`0.${req.params.tipPercentage}`);
     const newTotal = total * tipPercentage;
     
   
-    res.send('${newTotal}')
+    res.send(`${newTotal}`)
 })
+
+// Magic 8 Ball // 
+
+app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res) => {
+    const magic8BallResponses = [
+        "It is certain", 
+        "It is decidedly so", 
+        "Without a doubt", 
+        "Yes definitely",
+        "You may rely on it", 
+        "As I see it yes", 
+        "Most likely", 
+        "Outlook good",
+        "Yes", 
+        "Signs point to yes", 
+        "Reply hazy try again", 
+        "Ask again later",
+        "Better not tell you now", 
+        "Cannot predict now", 
+        "Concentrate and ask again",
+        "Don't count on it", 
+        "My reply is no", 
+        "My sources say no",
+        "Outlook not so good", 
+        "Very doubtful"
+    ];
+    const randIdx = Math. floor(Math. random()*20);
+    
+    
+    
+    
+    res.send('<h1>Will I Be A Millionaire?'$(magic8BallResponses))
+})
+
+
 
 
 
